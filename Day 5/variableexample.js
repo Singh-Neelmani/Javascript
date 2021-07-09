8ad44302f98d3a121f75a6d23245c1c983ffde17
Singh-Neelmani <neelmani.singh@timesinternet.in>
2021-07-09 12:50:52 +0530

    variableexamplejs01

---
+++ Day 5/variableexample.js
@@ -0,0 +1,14 @@
+var two =2;
+var ten=10;
+var linebreak="<br />";
+document.write("two plus ten= </body>");
+var result = two+ten;
+document.write(result);
+document.write(linebreak);
+document.write("ten*ten=");
+result=ten*ten;
+document.write(result);
+document.write(linebreak);
+document.write("ten/two= ");
+result=ten/two;
+document.write(result);
